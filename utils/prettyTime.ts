4f3c7946dae23560c0b3efc95eee722028c6a1cf
diegomarrufo <dmarrufo@ucm.es>
2022-08-30 00:28:26 +0200

    added default value for hours, minutes and seconds

--- utils/prettyTime.ts
+++ utils/prettyTime.ts
@@ -1,16 +1,18 @@
 export const prettyTime = (timeInSeconds: number) => {
 	const formatTime = (number: number) => number.toString().padStart(2, "0");
 
-	// ? add a default 0 to the map instead of the as number
 	const timeMap = new Map([
 		["hours", ~~(timeInSeconds / 3600)],
 		["minutes", ~~((timeInSeconds % 3600) / 60)],
 		["seconds", ~~timeInSeconds % 60],
 	]);
 
-	const hours = timeMap.get("hours") as number;
-	const minutes = timeMap.get("minutes") as number;
-	const seconds = timeMap.get("seconds") as number;
+	const [hours, minutes, seconds] = [
+		timeMap.get("hours") || 0,
+		timeMap.get("minutes") || 0,
+		timeMap.get("second") || 0,
+	];
 
+	// ? Only return hours if it exist, otherwise return minutes and seconds because it looks cleaner
 	return `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
 };
